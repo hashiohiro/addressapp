7bcd5b03ddaa76aed9842cd9102d71cfd74c5c33
hiroshi.hashimoto <hashiohiro@gmail.com>
2017-03-25 21:02:27 +0900

    JsonObjectMapperにJsonをデシリアライズするためのメソッドを追加した

--- app/main/infra/JsonObjectMapper.scala
+++ app/main/infra/JsonObjectMapper.scala
@@ -1,8 +1,11 @@
 package main.infra
 
+import com.fasterxml.jackson.core.`type`.TypeReference
+
 object JsonObjectMapper {
   private val mapper = new com.fasterxml.jackson.databind.ObjectMapper
   private val scalaModule = com.fasterxml.jackson.module.scala.DefaultScalaModule
+  private val typeRef = new TypeReference[Map[String, Any]] {}
   mapper.registerModule(scalaModule)
   
   /**
@@ -10,4 +13,10 @@ object JsonObjectMapper {
    * @param value
    */
   def writeValueAsString(value:AnyRef):String = mapper.writeValueAsString(value)
+  
+  /**
+   * JSONをMapにデシリアライズします。
+   * @param json
+   */
+  def readValue(json: String): Map[String, Any] = mapper.readValue(json, typeRef)
 }
